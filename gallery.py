46c99c669c3b8aaf8c0df1d4bcdcc8dbd0063c8a
bobbicandel <bobbicandel@gmail.com>
2026-03-05 11:50:45 +0800

    Create gallery.py

---
+++ gallery.py
@@ -0,0 +1,17 @@
+import json
+
+def update():
+
+    with open("history.json") as f:
+        history = json.load(f)
+
+    count = history["count"]
+
+    lines = []
+    lines.append("# Islamic Quote Posters\n")
+
+    for i in range(count,0,-1):
+        lines.append(f"![](poster/{i}.png)\n")
+
+    with open("README.md","w") as f:
+        f.write("\n".join(lines))
