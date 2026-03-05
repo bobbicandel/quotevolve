81aaa7f18daf7818554c882d96047fa4c3167890
bobbicandel <bobbicandel@gmail.com>
2026-03-05 11:47:15 +0800

    Create quote.py

---
+++ quote.py
@@ -0,0 +1,31 @@
+import random
+
+QUOTES = [
+{
+"text":"Sesungguhnya bersama kesulitan ada kemudahan.",
+"source":"QS Al-Insyirah 5-6"
+},
+{
+"text":"Allah tidak membebani seseorang melainkan sesuai kesanggupannya.",
+"source":"QS Al-Baqarah 286"
+},
+{
+"text":"Janganlah berputus asa dari rahmat Allah.",
+"source":"QS Az-Zumar 53"
+},
+{
+"text":"Sebaik-baik manusia adalah yang paling bermanfaat bagi manusia.",
+"source":"HR Bukhari"
+},
+{
+"text":"Allah itu indah dan menyukai keindahan.",
+"source":"HR Muslim"
+},
+{
+"text":"Bertakwalah kepada Allah di mana pun kamu berada.",
+"source":"HR Tirmidzi"
+}
+]
+
+def get():
+    return random.choice(QUOTES)
