33ca1e00599f4a28b94fb7a69f1259333f3e375f
bobbicandel <bobbicandel@gmail.com>
2026-03-05 12:18:45 +0800

    Update organism.py

--- organism.py
+++ organism.py
@@ -4,19 +4,45 @@ import random
 
 SIZE = 1200
 
-def fractal():
 
-    x = np.linspace(-2,2,SIZE)
-    y = np.linspace(-2,2,SIZE)
+def noise():
+
+    return np.random.rand(SIZE,SIZE)
+
+
+def rings():
+
+    x = np.linspace(-1,1,SIZE)
+    y = np.linspace(-1,1,SIZE)
 
     X,Y = np.meshgrid(x,y)
 
-    Z = (
-        np.sin(X*3)
-        + np.cos(Y*3)
-        + np.sin((X**2+Y**2)*4)
-        + np.cos((X**2-Y**2)*2)
-    )
+    R = np.sqrt(X**2 + Y**2)
+
+    Z = np.sin(R*15)
+
+    return Z
+
+
+def waves():
+
+    x = np.linspace(0,6*np.pi,SIZE)
+    y = np.linspace(0,6*np.pi,SIZE)
+
+    X,Y = np.meshgrid(x,y)
+
+    Z = np.sin(X)+np.cos(Y)
+
+    return Z
+
+
+def plasma():
+
+    a = rings()
+    b = waves()
+    c = noise()
+
+    Z = a*0.6 + b*0.3 + c*0.3
 
     Z = (Z - Z.min())/(Z.max()-Z.min())
 
@@ -26,11 +52,13 @@ def fractal():
 def palette():
 
     palettes = [
-        ((0,255,255),(255,0,200)),
-        ((255,80,0),(255,220,0)),
-        ((0,200,255),(120,0,255)),
-        ((0,255,120),(0,60,255)),
-        ((255,0,120),(255,255,0))
+
+        ((0,255,255),(0,40,120),(255,0,180)),
+        ((255,120,0),(255,0,80),(60,0,140)),
+        ((0,255,150),(0,60,255),(10,10,30)),
+        ((255,0,120),(255,220,0),(0,0,0)),
+        ((0,200,255),(120,0,255),(0,0,0)),
+
     ]
 
     return random.choice(palettes)
@@ -38,13 +66,13 @@ def palette():
 
 def generate():
 
-    Z = fractal()
+    Z = plasma()
 
-    c1,c2 = palette()
+    c1,c2,c3 = palette()
 
     r = (Z*c1[0] + (1-Z)*c2[0]).astype(np.uint8)
     g = (Z*c1[1] + (1-Z)*c2[1]).astype(np.uint8)
-    b = (Z*c1[2] + (1-Z)*c2[2]).astype(np.uint8)
+    b = (Z*c1[2] + (1-Z)*c3[2]).astype(np.uint8)
 
     img = np.stack([r,g,b],axis=2)
 
